525324788e60052609e6912ad09b8e14c650b9b4
sayyidahakindele <akindelesayyidah@gmail.com>
2024-10-19 11:30:44 -0400

    Challenge 4: updating 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,17 @@
 import { Central as Layout } from "@/layouts";
+import { Link } from "react-router-dom";
 import "./NotFound.style.scss";
 
 function NotFound() {
   return (
-    <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+    <Layout title={""}>
+      <h1 className="nf404">Oops!</h1>
+      <h2 className="nf404">404 - PAGE NOT FOUND</h2>
+      <p className="nf404">The page you're looking for might have been removed,</p>
+      <p className="nf404">had its name changed or is temporarily unavailable</p>
+      <button className="nf404">
+        <Link to={"/"}>Return To Menu</Link>
+      </button>
     </Layout>
   );
 }
