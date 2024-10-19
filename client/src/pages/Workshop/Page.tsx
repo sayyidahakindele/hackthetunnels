c636417c8a83bcdf921032362af9ebb9f0bb0c38
melissa <nguyenmelissa03@gmail.com>
2024-10-19 11:43:56 -0400

    workshop practice

--- client/src/pages/Workshop/Page.tsx
+++ client/src/pages/Workshop/Page.tsx
@@ -4,21 +4,31 @@ import Component from "./Component";
 function Page() {
   return (
     <div>
-      <h1>Example Page</h1>
+      <h1>Example Page
+        <button className="button">bello</button>
+      </h1>
+      <h2>BELLO</h2>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        BELLO!
       </p>
       <Component label="Label 1" />
       <Component label="Label 2" />
       <Component label="Label 3" />
+      <input type="date"/>
+      <button className="button1">click me 
+        <div>poo</div>
+      </button>
+      <div>
+        {/* function Page(){
+          const [searchText, setSearchText] = useState("default");
+
+          const helloWorld = () => {
+            setSearchText
+          }
+````````}
+        <input>type="text" placeholder="search.."</input>
+        <button onClick={helloWord}>Search</button> */}
+      </div>
     </div>
   );
 }
