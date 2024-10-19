5be77e87dcb6d9a4d13d0c52968c4bbb00f79d4a
gwookie <146489361+gwookie@users.noreply.github.com>
2024-10-19 15:33:49 -0400

    Make the emoji spin around...

--- client/src/layouts/Central/Banner/Banner.tsx
+++ client/src/layouts/Central/Banner/Banner.tsx
@@ -6,7 +6,7 @@ function Banner() {
       <img src="/carleton_logo.png" alt="logo" />
       <div className="Banner__seperator"></div>
       <div className="Banner__title">Carleton Central</div>
-      <img src="https://static-00.iconduck.com/assets.00/nerd-face-emoji-2048x2048-toyasgnd.png" alt="pic"/>
+      <img src="https://static-00.iconduck.com/assets.00/nerd-face-emoji-2048x2048-toyasgnd.png" className="rotate" width="100" height="100"/>
     </div>
   );
 }
