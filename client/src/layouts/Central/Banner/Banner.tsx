8b52151e1884f81fb2522fcdac034f2ad5bc634b
h31st <146489361+gwookie@users.noreply.github.com>
2024-10-19 13:34:32 -0400

    Challenge 3 - updated header, title and central colours

--- client/src/layouts/Central/Banner/Banner.tsx
+++ client/src/layouts/Central/Banner/Banner.tsx
@@ -6,6 +6,7 @@ function Banner() {
       <img src="/carleton_logo.png" alt="logo" />
       <div className="Banner__seperator"></div>
       <div className="Banner__title">Carleton Central</div>
+      <img src="https://static-00.iconduck.com/assets.00/nerd-face-emoji-2048x2048-toyasgnd.png" alt="pic"/>
     </div>
   );
 }
